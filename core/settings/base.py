28956ecba60df1af2f3f0e7cf5809576654998c4
jakhongirs <jakhongirsv@gmail.com>
2025-03-02 00:47:30 +0500

    featt: uz

--- core/settings/base.py
+++ core/settings/base.py
@@ -150,7 +150,7 @@ LOGGING = {
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "uz"
 
 LANGUAGES = (
     ("en", "English"),
