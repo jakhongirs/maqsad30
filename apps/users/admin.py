71739bff662b1e3e4fbb12fe5828ae91393aec84
jakhongirs <jakhongirsv@gmail.com>
2025-03-04 17:51:26 +0500

    feat: created_at

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,7 +5,7 @@ from .models import Timezone, User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email")
+    list_display = ("id", "username", "email", "created_at")
     list_filter = ("is_active", "is_staff")
     search_fields = ("username", "email", "first_name", "last_name")
 
